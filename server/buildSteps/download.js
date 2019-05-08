21da3dd53664dbb15d869b825b96ae5a65339e4c
rejas <github@veeck.de>
2019-05-08 22:01:40 +0200

    Update bugsnag and brwoserify

--- server/buildSteps/download.js
+++ server/buildSteps/download.js
@@ -1,10 +1,11 @@
 'use strict';
 var React = require('react');
+var ReactDOMServer = require('react-dom/server');
 var Modernizr = require('modernizr');
 
 var DownloadModule = require('../../frontend/js/download/DownloadUI');
 var downloadFactory = React.createFactory(DownloadModule);
 var options = require('../util/modernizrOptions');
 
-// render a static version of the `builder` for a faster inital render/script-less clients
-module.exports = React.renderToString(downloadFactory({detects:Modernizr.metadata(), options: options}));
+// render a static version of the `builder` for a faster initial render/script-less clients
+module.exports = ReactDOMServer.renderToString(downloadFactory({detects:Modernizr.metadata(), options: options}));
