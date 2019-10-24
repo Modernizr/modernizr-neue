16e52591e8566c66f26a027131e418e57a7a7c73
Veeck <veeck@neofonie.de>
2019-10-24 17:58:30 +0200

    Hey server, this content is for you! [skip ci]

---
+++ js/download/workers/build.js
@@ -0,0 +1,2 @@
+"use strict";var builder;builder=function(){var args=arguments;setTimeout(function(){builder.apply(this,args)},1e3)},onmessage=function(msg){var run=JSON.parse(msg.data);run.build?builder(run.build,function(build){postMessage(build)}):run.requireConfig&&(self._modernizrConfig=run.requireConfig,self._modernizrMetadata=run.metadata,importScripts("/lib/modernizr/lib/build.js"),require(["build"],function(build){builder=build}))},importScripts("/js/lodash.custom.js","/lib/r.js/dist/r.js"),define("lodash",function(){return _});
+//# sourceMappingURL=build.js.map
